ea450038122e1c71ab68f8cc1ce0e49ae6cb9807
vca <vca@somewhere.com>
2020-01-29 20:47:31 +0100

    work on demo

--- demo/http/reqhandler.c
+++ demo/http/reqhandler.c
@@ -22,39 +22,65 @@
  * SOFTWARE.
  */
 
-#include "task.h"
-#include "card.h"
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/time.h>
 #include <sys/types.h>
+#include <sys/socket.h>
 #include <unistd.h>
+
+#include "task.h"
+#include "card.h"
 
 /* data associated to a fiber */
 typedef struct extra_s {
-  int fd;
-  int pause;
+  int fd;       /* socket attached to fiber */
+  int pause;    /* time to pause between each image sent */
+  int hasdata;  /* tells if data is ready on socket */
 } extra_t;
 
 static char *boundary = "--boundarydonotcross";
 
-/* defined in main.c */
+/* this set contains the current sockets to watch */
+static fd_set cnxset;
+static int  cnxmaxfd = 0;
+
+#define CNXMAX 64
+static fiber_t *cnx2fiber[CNXMAX];
+
+/* all the cards, defined in main.c */
 extern card_t *allcards[];
 
+/* --------------------------------------------------------------------------
+ *  Get the socket of fiber
+ * --------------------------------------------------------------------------*/
 static int get_fiber_fd( fiber_t *f )
 {
   extra_t *data = (extra_t*) fiber_get_extra(f);
   return data->fd;
 }
 
+/* --------------------------------------------------------------------------
+ *  Get the pause flag of fiber
+ * --------------------------------------------------------------------------*/
 static int get_fiber_pause( fiber_t *f )
 {
   extra_t *data = (extra_t*) fiber_get_extra(f);
   return data->pause;
 }
 
-/* Write, checking for errors. */
+/* --------------------------------------------------------------------------
+ *  Set the hasdata flag of fiber
+ * --------------------------------------------------------------------------*/
+static void set_fiber_hasdata( int val )
+{
+  extra_t *data = (extra_t*) fiber_get_extra(f);
+  extra->hasdata = val;
+}
+
+/* --------------------------------------------------------------------------
+ *  Write, checking for errors.
+ * --------------------------------------------------------------------------*/
 static void safewrite(int fd, const char *b, size_t n)
 {
   size_t w = 0;
@@ -68,6 +94,9 @@ static void safewrite(int fd, const char *b, size_t n)
   }
 }
 
+/* --------------------------------------------------------------------------
+ *  Write a line
+ * --------------------------------------------------------------------------*/
 void writeln( int fd, char *fmt, ... )
 {
   va_list va;
@@ -80,6 +109,10 @@ void writeln( int fd, char *fmt, ... )
   safewrite( fd, "\n", 1);
 }
 
+/* --------------------------------------------------------------------------
+*  Send HTTP return code
+* Only 2 codes supported
+* --------------------------------------------------------------------------*/
 static void send_respose( int fd, int code )
 {
   switch( code ) {
@@ -92,11 +125,17 @@ static void send_respose( int fd, int code )
   }
 }
 
+/* --------------------------------------------------------------------------
+ *  Returns data string
+ * --------------------------------------------------------------------------*/
 static char *date()
 {
   return "Mon, 1 Jan 2030 12:34:56 GMT";
 }
 
+/* --------------------------------------------------------------------------
+ *  Send headers
+ * --------------------------------------------------------------------------*/
 static void request_headers( int fd )
 {
   writeln( fd, "Cache-Control: no-store, no-cache, must-revalidate, pre-check=0, post-check=0, max-age=0");
@@ -107,6 +146,9 @@ static void request_headers( int fd )
   writeln( fd, "" );
 }
 
+/* --------------------------------------------------------------------------
+ *  Send image headers
+ * --------------------------------------------------------------------------*/
 static void image_headers( int fd, card_t *card )
 {
   writeln( fd, "X-Timestamp: time.time()");
@@ -116,7 +158,10 @@ static void image_headers( int fd, card_t *card )
   writeln( fd, "" );
 } 
 
-void image_run( fiber_t *fiber )
+/* --------------------------------------------------------------------------
+ *  Send multipart image
+ * --------------------------------------------------------------------------*/
+void image( fiber_t *fiber )
 {
   card_t *card;
   int pause, fd = get_fiber_fd(fiber);
@@ -146,20 +191,31 @@ void image_run( fiber_t *fiber )
   }
 }
 
-void error404_run( fiber_t *fiber )
+/* --------------------------------------------------------------------------
+ *  Send error 404 reply
+ * --------------------------------------------------------------------------*/
+void error404( fiber_t *fiber )
 {
   int fd = get_fiber_fd(fiber);
-  
-  /* reply 404 and close connection */
   send_response( fd, 404);
 }
 
-void home_run( fiber_t *fiber )
+/* --------------------------------------------------------------------------
+ *  Send home page
+ * --------------------------------------------------------------------------*/
+void home( fiber_t *fiber )
 {
   int fd = get_fiber_fd(fiber);
+  char *page = "<html><head></head><body><img src='1.gif'></body></html>";
   
-  /* reply 200, send home page and close connection */
-  send_response( fd, 404);
+  send_response( fd, 200);
+  
+  writeln( fd, "Connection: close");
+  writeln( fd, "Content-Type: text/html");
+  writeln( fd, "Content-Length: %d", strlen(page)+1);
+  writeln( fd, "" );
+
+  writeln( fd, page);
 }
 
 /* --------------------------------------------------------------------------
@@ -169,51 +225,124 @@ void home_run( fiber_t *fiber )
  * --------------------------------------------------------------------------*/
 void generic_run( fiber_t *fiber )
 {
+  extra_t *extra = (extra_t*) fiber_get_extra( fiber );
+  int n, fd = get_fiber_fd( fiber );
+  char buffer[4096];
   char *location;
 
-  if ( strcmp(location, "/") == 0 ) {
-    home_run( fiber );
+  /* wait for incoming data */
+  extra->hasdata = 0;
+  ret = fiber_wait_for_var( fiber, MSEC, &extra->hasdata, 1);
+  if ( ret == FIBER_TIMEOUT ) {
+    continue;
   }
-  else if ( isdigit(location[0]) ) {
+
+  /* read request */
+  n = read( fd, buffer, sizeof(buffer));
+  puts(buffer);
+  if ( n > 0 ) {
+    if ( strncmp( buffer, "GET ", 4 ) == 0 ) {
+      if ( strcmp(location, "/") == 0 ) {
+	home( fiber );
+      }
+      else if ( isdigit(location[1]) ) {
+	image( fiber );
+      }
+      else {
+	error404( fiber );
+      }
+    }
+    else {
+      /* only GET supported */
+      error404( fiber );
+    }
   }
   else {
-    error404_run( fiber );
+    /* read returned 0, which means that the connection was closed
+     * just leave the infinite read loop. */
+    break;
   }
 }
 
+/* --------------------------------------------------------------------------
+ *  Called once fiber has finished running
+ * --------------------------------------------------------------------------*/
 void done( fiber_t *fiber )
 {
-  int fd = get_fiber_fd(fiber);
+  int i, fd = get_fiber_fd(fiber);
+  for ( i = 0; i < CNXMAX; ++i ) {
+    if ( cnx2fiber[i] == fiber ) {
+      cnx2fiber[i] = NULL;
+    }
+  }
   close(fd);
+  FD_CLEAR( fd, &cnxfd );
+  /* recompute cnxmaxfd if needed */
+  if ( cnxmaxfd == fd ) {
+    for( i = cnxmaxfd = 0; i < CNXMAX; ++i ) {
+      if ( cnx2fiber[i] != NULL ) {
+	fd = fiber_get_fd( cnx2fiber[i] );
+	if ( fd > cnxmaxfd ) {
+	  cnxmaxfd = fd;
+	}
+      }
+    }
+  }
+  /* safe to free fiber. Its stack has already been deallocated */
+  free( fiber->extra );
+  free( fiber );
 }
 
 
-int seloop(void) {
+/* --------------------------------------------------------------------------
+ *  Call select to find which connection has pending data to read
+ *  Arrange for the fiber handling th connection to wake up.
+ *
+ *  This function is called at the beginnig of each scheduler cycle.
+ * --------------------------------------------------------------------------*/
+int selectloop(void) {
   fd_set rfds;
   struct timeval tv;
-  int retval;
+  int i, retval;
 
-  /* Watch stdin (fd 0) to see when it has input. */
+  /* Watch sockets to see when they have input. */
   FD_ZERO(&rfds);
-  FD_SET(0, &rfds);
+  memcpy( &rdfs, &cnxfs, sizeof(rdfs) );
 
-  /* Wait up to five seconds. */
-  tv.tv_sec = 5;
-  tv.tv_usec = 0;
+  /* Wait up to 50 msecs. */
+  tv.tv_sec = 0;
+  tv.tv_usec = 5000;
 
-  retval = select(1, &rfds, NULL, NULL, &tv);
+  retval = select( cnxmaxfd+1, &rfds, NULL, NULL, &tv);
   /* Don't rely on the value of tv now! */
 
-  if (retval == -1)
+  if (retval == -1) {
     perror("select()");
-  else if (retval)
+    
+  } else if (retval) {
     printf("Data is available now.\n");
-  /* FD_ISSET(0, &rfds) will be true. */
-  else
+
+    /* loop over connections */
+    for( i = 0; i < CNXMAX; ++i ) {
+      fiber_t *fiber = cnx2fiber[i];
+      if ( fiber != NULL ) {
+	extra_t *extra = fiber_get_extra( fiber );
+	int fd = get_fiber_fd( fiber );
+	if ( FD_ISSET( fd, &rdfs) ) {
+	  extra->hasdata = 1;  /* it will wake up the fiber */
+	}
+      }
+    }
+    
+  } else {
     printf("No data within five seconds.\n");
+  }
 }
 
-void mktask( schedulter_t *sched, int fd )
+/* --------------------------------------------------------------------------
+ *  Create a task to handle connection
+ * --------------------------------------------------------------------------*/
+void mktask( scheduler_t *sched, int fd )
 {
   extra_t *pextra;
   fiber_t *fiber;
@@ -221,13 +350,70 @@ void mktask( schedulter_t *sched, int fd )
   extra = (extra_t*) malloc(sizeof(extra_t));
   extra->fd = fd;
   extra->pause = 200 + (rand() % 800); /* pause between 200ms to 1s */
+  extra->hasdata = 0;
+
+  FD_SET( fd, &cnxset );
+  if ( fd > cnxmaxfd ) {
+    cnxmaxfd = fd;
+  }
+  
   fiber = fiber_new( generic_run, extra);
-  fiber_set_stack_size( fiber, 1024);
+  fiber_set_stack_size( fiber, 8192);
   fiber_set_done_func( fiber, done);
   fiber_start( sched, fiber);
 }
 
+/* --------------------------------------------------------------------------
+ *  accept_task
+ *  Called when select find some data
+ * --------------------------------------------------------------------------*/
+void accept_task( fiber_t *fiber )
+{
+  int ret, newfd, fd = get_fiber_fd( fiber );
+  extra_t *extra = (extra_t*) fiber_get_extra( fiber );
 
+  while(1) {
+    /* wait for incoming data */
+    extra->hasdata = 0;
+    ret = fiber_wait_for_var( fiber, MSEC, &extra->hasdata, 1);
+    if ( ret == FIBER_TIMEOUT ) {
+      continue;
+    }
 
+    /* create new connection and new fiber to serve it */
+    newfd = accept(fd, NULL, NULL);
+    if ( newfd >= 0 ) {
+      mktask( fiber_get_scheduler( fiber ), newfd );
+    }
+    else {
+      perror("accept()");
+    }
+  }
+}
 
-  
+/* --------------------------------------------------------------------------
+ *  server
+ *  Opens the server socket, create the scheduler and the initial task
+ *  that will handle
+ * --------------------------------------------------------------------------*/
+void server()
+{
+  scheduler_t *sched;
+  fiber_t *fiber;
+
+  /* create listening socket */
+
+  /* create scheduler */
+  sched = scheduler_new();
+
+  /* create the fiber that will accept
+   * incoming connections */
+  fiber = fiber_new( accept_task);
+  fiber_set_stack_size( fiber, 4096);
+  fiber_start( sched, fiber );
+
+  while(1) {
+    selectloop();
+    scheduler_cycle( sched );
+  }
+}
